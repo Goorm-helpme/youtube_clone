953320102d0e98baac678f80dc382c450bf335a8
WhiteKIM <9rudrb40@naver.com>
2023-08-01 21:19:58 +0900

    2023.08.01 김경규
    대댓글 수정시 버튼이 잘못 생성되는 문제 해결, 대댓글 수정후 현재 페이지로 새로고침 추가 완료

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,7 +1,9 @@
-function replyUpdateButton() {
-     document.getElementById('replyPostContent').readOnly = false;
-     const container = document.querySelector('.replyMode')
+var replyid;
+function replyUpdateButton(id) {
+     document.getElementById('replyPostContent-'+id).readOnly = false;
+     const container = document.getElementById('replyMode-'+id);
      var button = '<button class="btn ReplyPosting" onclick="replyUpdate()">등록</button>'
+     replyid = id;
      container.innerHTML += button;
 }
 
@@ -22,16 +24,17 @@ function replyDelete(id, parentId) {
 
 function replyUpdate() {
     let data = {
-        id : document.getElementById("reply-id").value,
-        content : document.getElementById("replyPostContent").value,
+        id : document.getElementById("reply-id-"+replyid).value,
+        content : document.getElementById("replyPostContent-"+replyid).value,
     }
-    let videoId = document.querySelector(".video-id").value;
+    var videoId = document.getElementById('video-id').value;
+    console.log(videoId);
     httpRequest = new XMLHttpRequest();
     httpRequest.open("POST","/reply/update", true);
     httpRequest.responseType = "json";
     httpRequest.setRequestHeader("Content-Type","application/json");
     httpRequest.send(JSON.stringify(data));
     setTimeout(function(){
-        window.location.href="/video?id=" + videoId;
+        window.location.href="/video?id="+videoId;
     }, 300);
 }
